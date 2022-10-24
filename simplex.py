0886f6ed4bb9f538bf75a259328c0b63c5d7b9d1
thiago_padua <thiago.padua@cinnecta.com>
2022-10-24 15:55:42 -0300

    receivinginput

---
+++ simplex.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+restrictions, variables = int(input()).split()
+
+restrictions_matrix = np.array([])
+current_line = []
+for 
+for i in range(0, variables):
+    current_line.append()
+
