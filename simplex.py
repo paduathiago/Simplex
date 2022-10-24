4b23e5f1ab0aea7f671879d8f0d54856b74dc10a
thiago_padua <thiago.padua@cinnecta.com>
2022-10-24 16:06:10 -0300

    adding function to print matrix

--- simplex.py
+++ simplex.py
@@ -1,10 +1,21 @@
 import numpy as np
 
+def print_matrix(matrix):
+    for i in np.shape(matrix)[0]:  # rows
+        for j in np.shape(matrix)[1]:
+            print(f'{matrix[i][j]} ')
+        print()
+
+
 restrictions, variables = int(input()).split()
 
 restrictions_matrix = np.array([])
-current_line = []
-for 
-for i in range(0, variables):
-    current_line.append()
+current_line = np.array([])
+for k in range(restrictions):
+    for value in input().split():
+        current_line.append(value)
+    restrictions_matrix.append(current_line, axis = 0)
+    current_line = np.array([])
+
+
 
